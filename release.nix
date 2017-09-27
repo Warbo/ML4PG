8900038191e5014ad5b80d78ea1e53dbfe867c98
Chris Warburton <chriswarbo@gmail.com>
2017-09-27 14:21:21 +0100

    Remove 'override' and 'overrideDerivation' junk from unstable

--- release.nix
+++ release.nix
@@ -1,9 +1,17 @@
 {
+  # Uses stable defaults from default.nix
   stable   = import ./. { packageOnly = false; };
-  unstable = with import <nixpkgs> {};
-             callPackage ./. {
-               emacsWithPG = emacsWithPackages [
-                               emacsPackages.proofgeneral ];
-               packageOnly = false;
-             };
+
+  # Uses whichever <nixpkgs> is in scope
+  unstable =
+    with import <nixpkgs> {};
+    with rec {
+      emacsWithPG = emacsWithPackages [ emacsPackages.proofgeneral ];
+      pkgWithJunk = callPackage ./. {
+                      inherit emacsWithPG;
+                      packageOnly = false;
+                    };
+    };
+    # callPackage gives us 'override' attributes which annoys Hydra
+    builtins.removeAttrs pkgWithJunk [ "override" "overrideDerivation" ];
 }
